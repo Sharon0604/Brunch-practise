5a03286bda5e142d1713d6514b3d7e30ff01b518
Sharon0604 <112067703+Sharon0604@users.noreply.github.com>
2023-02-16 11:15:49 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+
 var swiper = new Swiper(".home-slider", {
     loop:true,
     effect: "coverflow",
